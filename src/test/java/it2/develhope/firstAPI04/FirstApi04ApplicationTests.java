4f4feb643c720d38945a0089a26c139380d15d62
Angelo Zammataro <juba.kem@gmail.com>
2022-07-22 09:29:31 +0200

    First Commit: Primi passi - First API - 04

---
+++ src/test/java/it2/develhope/firstAPI04/FirstApi04ApplicationTests.java
@@ -0,0 +1,13 @@
+package it2.develhope.firstAPI04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApi04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
